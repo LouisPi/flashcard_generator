aeae142202fcac2cd40ba703a75c059ed2f50111
Louis Pi <granbobbi22@gmail.com>
2019-02-15 20:27:03 +0100

    checking pasting images works fine - it does

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
-page_1 = Image.open("page_1.png")
-page_2 = Image.open("page_2.png")
-page_1 = page_1.resize((2480, 3508))
-page_2 = page_2.resize((2480, 3508))
+page_1 = Image.new('RGB', (2480, 3508))
+page_2 = Image.new('RGB', (2480, 3508))
+image_1 = Image.new('RGB', (2480/2, 3508/2), color = 'green')
+image_2 = Image.new('RGB', (2480/2, 3508/2), color = 'yellow')
+image_3 = Image.new('RGB', (2480/2, 3508/2), color = 'red')
+image_4 = Image.new('RGB', (2480/2, 3508/2), color = 'orange')
+image_5 = Image.new('RGB', (2480/2, 3508/2), color = 'black')
+image_6 = Image.new('RGB', (2480/2, 3508/2), color = 'white')
+image_7 = Image.new('RGB', (2480/2, 3508/2), color = 'purple')
+image_8 = Image.new('RGB', (2480/2, 3508/2), color = 'pink')
 
-page_1.save("page_1.pdf", "PDF", resolution=300.0, save_all=True, append_images=[page_2])
+# d1 = ImageDraw.Draw(page_1)
+# d2 = ImageDraw.Draw(page_2)
+# d1.text((10,10), "Hello world", fill=(255,255,0))
+# d2.text((10,10), "Hello world", fill=(255,255,0))
+
+page_1.paste(image_1, (0, 0))
+page_1.paste(image_2, (1240, 0))
+page_1.paste(image_3, (0, 1754))
+page_1.paste(image_4, (1240, 1754))
+page_2.paste(image_5, (0, 0))
+page_2.paste(image_6, (1240, 0))
+page_2.paste(image_7, (0, 1754))
+page_2.paste(image_8, (1240, 1754))
+
+page_1.save("flashcards.pdf", "PDF", resolution=300.0, save_all=True, append_images=[page_2])
